f76c7c09836e8f7e275d58e2401105db49a48bae
Dina Nur Auliana <dinakayla29@gmail.com>
2024-05-01 10:13:10 +0700

    clean architecture
    + Added file to folder utils

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,13 @@
+class TImages {
+  TImages._();
+
+  // -- APP LOGOS
+  static const String darkAppLogo =
+      'assets/logos/t-store-spalsh-logo-black.png';
+  static const String lightAppLogo =
+      'assets/logos/t-store-spalsh-logo-white.png';
+
+  // -- SOCIAL LOGOS
+  static const String google = 'assets/logos/google-icon.png';
+  static const String facebook = 'assets/logos/facebook-icon.png';
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/app.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_ecommerce/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
